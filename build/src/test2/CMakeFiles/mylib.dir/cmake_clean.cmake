a4780d3a85448ab908026d9a4ce554b6eb7281ab
lxr <jieyao.lxr@foxmail.com>
2023-06-25 20:28:34 +0800

    init

---
+++ build/src/test2/CMakeFiles/mylib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylib.dir/test.cpp.o"
+  "CMakeFiles/mylib.dir/test.cpp.o.d"
+  "libmylib.a"
+  "libmylib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mylib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
