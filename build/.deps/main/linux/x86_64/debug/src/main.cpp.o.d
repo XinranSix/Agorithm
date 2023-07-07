6dce426d6af8bddcb4a23621ceebac1c44cd409d
Yao J <jieyao.lxr@foxmail.com>
2023-07-07 09:44:02 +0800

    asd

--- build/.deps/main/linux/x86_64/debug/src/main.cpp.o.d
+++ build/.deps/main/linux/x86_64/debug/src/main.cpp.o.d
@@ -1,4 +1,9 @@
 {
+    depfiles_gcc = "main.o: src/main.cpp\
+",
+    files = {
+        "src/main.cpp"
+    },
     values = {
         "/usr/bin/gcc",
         {
@@ -9,10 +14,5 @@
             "-isystem",
             "/home/yaojie/.xmake/packages/f/fmt/10.0.0/cf4a2efd8ed34f738dfde8eaa4fb9660/include"
         }
-    },
-    files = {
-        "src/main.cpp"
-    },
-    depfiles_gcc = "main.o: src/main.cpp\
-"
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
-//声明模板类demo
-template<typename... Values> class demo;
-//继承式递归的出口
-template<> class demo<> {};
-//以继承的方式解包
-template<typename Head, typename... Tail>
-class demo<Head, Tail...>
-    : private demo<Tail...>
-{
+class Student {
 public:
-    demo(Head v, Tail... vtail) : m_head(v), demo<Tail...>(vtail...) {
-        dis_head();
-    }
-    void dis_head() { std::cout << m_head << std::endl; }
-protected:
-    Head m_head;
+    Student(bool g, int a) : gender(g), age(a) {}
+
+private:
+    bool gender;
+    int age;
 };
-int main() {
-    demo<int, float, std::string> t(1, 2.34, "http://www.biancheng.net");
-    return 0;
-}
+
+union T {
+    Student s; // 含有非POD类型的成员，gcc-5.1.0  版本报错
+    char name[10];
+};
+int main() { return 0; }
